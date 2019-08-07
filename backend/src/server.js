0e453117b5b4c1e5049786f40da1763cb3d4bc00
Nayara Ferreira <nayara.ferreira@tron.com.br>
2019-08-07 18:28:06 -0300

    070819: ajuste do projeto

---
+++ backend/src/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const server = express();
+const bodyParser = 
+
+server.get('/', (req, res) => {
+    //return res.send(`Hello ${req.query.name}`); 
+
+});
+
+server.listen(3333);
+
