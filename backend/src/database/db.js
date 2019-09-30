53ff164e5c17e4f67c2b76539eeb1d44d84f558a
Nayara - Home <ncy.fers@gmail.com>
2019-09-30 11:18:55 -0300

    30092019-01: ajuste de código para iniciar o projeto da semana Omnistack 30/09 a 04/10

--- backend/src/database/db.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://<username>:<password>@clusterapi-6ummn.mongodb.net/test?retryWrites=true&w=majority', 
- {  useMongoClient: true });
