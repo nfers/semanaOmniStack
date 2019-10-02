cdab7f00d6006780521908a4ad16f6994d99c6dc
Nayara - Home <ncy.fers@gmail.com>
2019-10-01 22:18:35 -0300

    20191001-5: ajustes nas rotas da aplicação

--- backEnd/src/Routes/routes.js
+++ backEnd/src/Routes/routes.js
@@ -4,14 +4,20 @@ const multer = require('multer');
 const ImageConfig = require('../config/upload');
 const sessionController = require('../Controllers/sessionController');
 const spotController = require('../Controllers/spotController');
+const dashboardController = require('../Controllers/dashboardController');
+const bookingController = require('../Controllers/bookingController');
 
 const routes = express.Router();
 const upload = multer(ImageConfig);
 
 routes.post('/sessions', sessionController.store);
-routes.get('/spots', spotController.index);
-routes.post('/spots', upload.single('thumbnail'),spotController.store);
 
+routes.get('/spots', spotController.index);
+routes.post('/spots', upload.single('thumbnail'), spotController.store);
+
+routes.get('/dashboard', dashboardController.show);
+
+routes.post('/spots/:spot_id/bookings', bookingController.store);
 
 module.exports = routes;
 
