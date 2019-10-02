14027da3300282f2084a1a0c20fb8e1e69db93de
Nayara - Home <ncy.fers@gmail.com>
2019-10-01 21:49:28 -0300

    20191001-03: implementação dos controllers

---
+++ backEnd/src/Controllers/spotController.js
@@ -0,0 +1,35 @@
+const Spot = require('../Models/spot');
+const User = require('../Models/user');
+
+module.exports = {
+    async index(req, res) {
+       const { tech } = req.query;
+
+       const spots = await Spot.find({ tech: tech});
+
+       return res.json(spots);
+    },
+
+    async store(req, res) {
+        const { filename } = req.file;
+        const { company, techs, price } = req.body;
+        const { user_id } = req.headers;
+
+        const user = await User.findById(user_id);
+
+        if (!user) {
+            return res.status(400).json({ error: 'Que pena, o usuário não existe'});
+        }
+
+        const spot = await Spot.create({
+            user: user_id,
+            thumbnail: filename,
+            company, 
+            techs: techs.split(',').map(tech => tech.trim()),
+            price
+        })
+
+        return res.json(spot)
+    }
+    
+};
