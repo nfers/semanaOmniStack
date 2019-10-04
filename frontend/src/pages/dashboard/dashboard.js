07f30f765009b26c442657d5de8c6449a6ee4560
Nayara - Home <ncy.fers@gmail.com>
2019-10-03 21:49:06 -0300

    20191003-11: criacao das paginas de navegacao ( cadastro de novo espaço e Dashboard )

--- frontend/src/pages/dashboard/dashboard.js
+++ frontend/src/pages/dashboard/dashboard.js
@@ -1,5 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import api from '../../services/api';
+import './dashboard.css';
+
 
 export default function Dashboard() {
-    return <div></div>
+    const [ spots, setSpots] = useState([]);
+    useEffect(() => {
+        async function loadSpots() {
+            const user_id = localStorage.getItem('user');
+            const response = await api.get('http://localhost:3031/dashboard', {
+                headers: { user_id }
+            });
+            setSpots(response.data);
+        }
+        loadSpots();
+    }, []);
+
+    return ( 
+        <>
+            <ul className="spot-list">
+                {spots.map(spot  => (
+                    <li key={spot._id}>
+                        <header style={{ backgroundImage: `url(${spot.thumbnail_url})` }}/>
+                        <strong>{spot.company}</strong>
+                        <span>{spot.price ? `R${spot.price}/dia` :'Gratuito'}</span>
+                    </li>
+                ))}
+            </ul>
+
+            <Link to="/new">
+                Cadastrar novo Espaço
+            </Link>
+        </>
+    )
 }
+
+
+//20191003-10: ajuste no model spot
--- frontend/src/pages/new/newSpot.js
+++ frontend/src/pages/new/newSpot.js
@@ -1,5 +1,68 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import camera from '../../assets/camera.svg';
+import api from '../../services/api';
 
-export default function NewSpot() {
-    return <div></div>
+import './newSpot.css';
+
+export default function NewSpot({ history}) {
+    const [company, setCompany] = useState('');
+    const [techs, setTechs] = useState('');
+    const [price, setPrice] = useState('');
+    const [thumbnail, setthumbnail] = useState(null);
+
+    const preview = useMemo(() => {
+        return thumbnail ? URL.createObjectURL(thumbnail) : null;
+    }, [thumbnail])
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+
+        const data = new FormData();
+        const user_id = localStorage.getItem('user');
+
+        data.append('thumbnail', thumbnail);
+        data.append('company', company);
+        data.append('techs', techs);
+        data.append('price', price);
+
+        await api.post('http://localhost:3031/spots', data, {
+            headers: { user_id }
+        })
+
+        history.push('/dashboard');
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label id="thumbnail" style={{ backgroundImage: `url(${preview})` }}>
+                <input type="file" onChange={event => setthumbnail(event.target.files[0])}/>
+                <img src={camera} alt="Seleciona a imagem" />
+            </label>
+
+            <label htmlFor="company"> Empresa *</label>
+            <input 
+                id="company"
+                placeholder="Sua Empresa"
+                value={company}
+                onChange={event => setCompany(event.target.value)}
+            />
+            
+            <label htmlFor="techs"> Tecnologias * <span>(separadas por virgula)</span> </label>
+            <input 
+                id="techs"
+                placeholder="Quais são as tecnologias?"
+                value={techs}
+                onChange={event => setTechs(event.target.value)}
+            />
+
+            <label htmlFor="price"> Valor da Diaria * <span>(em branco se for gratuito)</span> </label>
+            <input 
+                id="price"
+                placeholder="Valor Cobrado por dia"
+                value={price}
+                onChange={event => setPrice(event.target.value)}
+            />
+            <button type="submit" className="btn"> Salvar</button>
+        </form>
+    )
 }
